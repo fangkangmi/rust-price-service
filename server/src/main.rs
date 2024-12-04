413575c72517a822b9f46d84b85a3cb1e8aa1ccf
Fangkang <fangkangmi@outlook.com>
2024-12-04 13:30:57 +0000

    refactor: restructure project into client and server workspaces with basic TCP communication

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,22 @@
+use std::{net::TcpListener, result};
+use std::io::{Read, Write};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    println!("Running on port 3000");
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Connection established!");
+
+        // A mutable buffer of 1024 bytes, initialized with zeros.
+        // This buffer can be used to store data temporarily during I/O operations.
+        let mut buffer = [0; 1024];
+
+        stream.read(&mut buffer).unwrap();
+        stream.write(&buffer).unwrap();
+
+    }
 }
