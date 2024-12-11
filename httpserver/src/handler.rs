0f09cd02c3be2527267d67f2565946f0daae9c50
Fangkang <fangkangmi@outlook.com>
2024-12-11 23:56:24 +0000

    feat: implement claims handling and add HTML responses for health and 404 pages

--- httpserver/src/handler.rs
+++ httpserver/src/handler.rs
@@ -7,6 +7,8 @@ use std::fs;
 pub trait Handler {
     fn handle(req: &HttpRequest) -> HttpResponse;
     fn load_file(file_name: &str) -> Option<Vec<u8>> {
+        // env! and env::var are used to get the value of the environment variable
+        // the difference is that env! will panic if the environment variable is not set
         let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
         let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
         let full_path = format!("{}/{}", public_path, file_name);
@@ -20,10 +22,10 @@ pub struct PageNotFoundHandler;
 pub struct WebServiceHandler;
 
 #[derive(Serialize, Deserialize)]
-pub struct OrderStatus {
-    order_id: i32,
-    order_date: String,
-    order_status: String,
+pub struct Myclaims {
+    item: String,
+    vat_incl: bool,
+    quantity: i32,
 }
 
 impl Handler for PageNotFoundHandler {
@@ -39,18 +41,20 @@ impl Handler for StaticPageHandler {
         };
         let route: Vec<&str> = s.split("/").collect();
         match route[1] {
+            // after localhost:3000/, if it is health, then call health.html
             "" => HttpResponse::new("200", None, Self::load_file("index.html")),
             "health" => HttpResponse::new("200", None, Self::load_file("health.html")),
+
+            // after localhost:3000/, if it is api, then call WebServiceHandler
             path => match Self::load_file(path) {
                 Some(contents) => {
                     let mut map: HashMap<&str, &str> = HashMap::new();
-                    if path.ends_with(".css") {
-                        map.insert("Content-Tvpe", "text/css");
-                    } else if path.ends_with(".js") {
+                    if path.ends_with(".js") {
                         map.insert("Content-Type", "text/javascript");
                     } else {
                         map.insert("Content-Type", "text/html");
                     }
+                    // return the contents of the file
                     HttpResponse::new("200", Some(map), Some(contents))
                 }
                 None => HttpResponse::new("404", None, Self::load_file("404.html")),
@@ -60,14 +64,15 @@ impl Handler for StaticPageHandler {
 }
 
 impl WebServiceHandler {
-    fn load_json() -> Vec<OrderStatus> {
+    fn load_json() -> Vec<Myclaims> {
         let default_path = format!("{}/data", env!("CARGO_MANIFEST_DIR"));
         let data_path = env::var("DATA_PATH").unwrap_or(default_path);
-        let full_path = format!("{}/{}", data_path, "orders.json");
+        let full_path = format!("{}/{}", data_path, "claims.json");
+        println!("Loading JSON from: {}", full_path);
         let json_contents = fs::read_to_string(full_path);
-        let orders: Vec<OrderStatus> =
+        let claims: Vec<Myclaims> =
             serde_json::from_str(json_contents.unwrap().as_str()).unwrap();
-        orders
+        claims
     }
 }
 
@@ -78,8 +83,9 @@ impl Handler for WebServiceHandler {
         };
         let route: Vec<&str> = s.split("/").collect();
 
+        //localhost:3000/api/price
         match route[2] {
-            "shipping" if route.len() > 2 && route[3] == "orders" => {
+            "price" if route.len() >2 => {
                 let body = serde_json::to_string(&Self::load_json()).unwrap().into_bytes();
                 let mut headers: HashMap<&str, &str> = HashMap::new();
                 headers.insert("Content-Type", "application/json");
